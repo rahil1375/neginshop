f356dfe5f0f4ae369a7f09a79eb1301b20e95804
rahil1375 <rahilkhosravi50@gmail.com>
2025-09-14 00:03:24 +0330

    ..

--- src/Components/EditProfile.jsx
+++ src/Components/EditProfile.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 function EditProfile(){
+
     return(
-        <div className="" style={{width:"100%",height:"40vh",backgroundColor:"coral",paddingTop:"30vh"}}>
-            edit profile
+        <div className="" style={{width:"100%",backgroundColor:"coral",paddingTop:"23vh",paddingBottom:"3vh",direction:"rtl",display:"flex",justifyContent:"center"}}>
+          <form className="content" style={{width:"96%",borderRadius:"20px",backgroundColor:"#ffff",display:"flex",flexWrap:"wrap",flexDirection:"row",justifyContent:"flex-start"}}>
+               <p>نام : <input type="textbox"></input></p>
+                <p>ایمیل : <input type="textbox"></input></p>
+                <p>پسورد : <input type="textbox"></input></p>
+                
+                <p>نقش کاربر : <select name="role">
+                    <option value="user">کاربر ساده</option>
+                    <option value="admin">ادمین</option>
+                </select></p>
+                <input type="file" accept="image/*" />
+
+                <button type="submit" style={{width:"fit-content",height:"fit-content"}}>ذخیره تغییرات</button>
+          </form>
         </div>
     )
 }
